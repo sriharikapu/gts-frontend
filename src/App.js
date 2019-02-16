344fa87ab92b572d7eed2dbb55998332b966bcc4
Piotr Adamczyk <xeno@xeno.yt>
2019-02-15 22:22:28 -0700

    basic navbar and  Fortmatic dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './utils/bootstrap.scss';
+
+import { Navbar, Nav } from 'react-bootstrap';
+import NavProfile from './components/NavProfile';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Navbar expand="sm" bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Global Trade System</Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse>
+      <Nav>
+      <Nav.Link>Offers</Nav.Link>
+      <Nav.Link>Inventory</Nav.Link>
+      <Nav.Link>Settings</Nav.Link>
+      <NavProfile />
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/components/NavProfile.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class NavProfile extends Component {
+    render() {
+        return "0x0xC79a5808Ede2DE4e1154235742f6266Dd5e48982";
+    }
+}
