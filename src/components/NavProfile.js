f990ecfe33fc243e763b2075c12104a24cd08dde
Piotr Adamczyk <xeno@xeno.yt>
2019-02-15 23:49:29 -0700

    new address display

--- src/components/NavProfile.js
+++ src/components/NavProfile.js
@@ -5,7 +5,21 @@ import Blockie from "react-blockies";
 import "./NavProfile.css";
 
 export default class NavProfile extends Component {
-    render() {
-        return <span><Blockie seed={window.web3.eth.defaultAccount} className="NavProfile-blockie" />{window.web3.eth.defaultAccount}</span>;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: null
+    };
+  }
+  async componentDidMount() {
+    this.setState({ address: (await window.web3.eth.getAccounts())[0] });
+  }
+  render() {
+    return (
+      <span>
+        <Blockie seed={this.state.address} className="NavProfile-blockie" />
+        {this.state.address}
+      </span>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import Fortmatic from "fortmatic";
 import Web3 from "web3";
 
-const fm = new Fortmatic('pk_test_7A6DD1EB8EE45B75');
+const fm = new Fortmatic("pk_test_7A6DD1EB8EE45B75");
 
-if(!window.web3)
-    window.web3 = new Web3(fm.getProvider());
+// Post EIP-1102 update which MetaMask no longer injects web3
+if (window.ethereum) {
+  // Use MetaMask provider
+  window.web3 = new Web3(window.ethereum);
+} else {
+  // Use Fortmatic provider
+  window.web3 = new Web3(fm.getProvider());
+}
 
-if(window.ethereum)
-    window.ethereum.enable();
+if (window.web3.currentProvider.enable) window.web3.currentProvider.enable();
+if (window.ethereum.enable) window.ethereum.enable();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (window.web3) {
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
