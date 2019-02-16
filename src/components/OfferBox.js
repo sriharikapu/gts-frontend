6248b936337edb7e22090215117cd7f7d07ebd2e
Jonbi2 <marcellobardus8@gmail.com>
2019-02-16 15:37:15 -0700

    offers touched

---
+++ src/components/OfferBox.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import "./OfferBox.css";
+
+export default class OfferBox extends Component {
+  render() {
+    return <div className="OfferBox">OfferBox</div>;
+  }
+}
--- src/views/OffersView.js
+++ src/views/OffersView.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
 
 export default class OffersView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { offers: null };
+  }
+  componentDidMount() {
+    this.refresh();
+  }
+  async refresh() {
+    this.setState({ offers: null });
+    const account = (await window.web3.eth.getAccounts())[0];
+    this.setState({
+      items: await GTS.methods
+        .getMyOffers()
+        .call({ from: account, gasLimit: 10000000 })
+    });
+  }
+
   render() {
-    return <div>OffersView</div>;
+    return (
+      <div className="OffersView">
+        {" "}
+        <button onClick={() => this.refresh()}>Refresh</button>
+        {this.state.items
+          ? this.state.items.map(i => <ItemBox key={i} id={i} />)
+          : "Loading..."}
+      </div>
+    );
   }
 }
