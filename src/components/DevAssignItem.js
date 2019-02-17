e31a3ea62ca5b6a063f063ea311f38fd3150e72c
Piotr Adamczyk <xeno@xeno.yt>
2019-02-17 03:24:04 -0700

    devassignitem initial commit

---
+++ src/components/DevAssignItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DevAssignItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: null
+    };
+  }
+  render() {
+    return <div className="DevAssignItem" />;
+  }
+}
