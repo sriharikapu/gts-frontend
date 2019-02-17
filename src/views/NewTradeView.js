8f038e9409c43b038409a0053ea663be589754d3
Piotr Adamczyk <xeno@xeno.yt>
2019-02-17 02:24:51 -0700

    fixed refresh of newtradeview's children

--- src/views/NewTradeView.js
+++ src/views/NewTradeView.js
@@ -58,134 +58,146 @@ export default class OffersView extends Component {
     });
   }
   render() {
-    return React.createElement(
-      withRouter(({ history }) => (
-        <div className="NewTradeView">
-          <Container className="mt-4">
-            <Row>
-              <Col>
-                <Button
-                  variant="danger"
-                  className=""
-                  onClick={() => history.push("/offers")}
-                >
-                  Back
-                </Button>
-              </Col>
-            </Row>
-            <Row className="mt-2">
-              <Col md="6">
-                <InputGroup>
-                  <FormControl disabled value={this.state.myAddress} readOnly />
-                  <InputGroup.Append>
-                    <Button
-                      variant="secondary"
-                      onClick={() => this.refreshMyInventory()}
-                    >
-                      Refresh
-                    </Button>
-                  </InputGroup.Append>
-                </InputGroup>
-                <div className="mt-2">
-                  <Inventory
-                    items={this.state.myAssets}
-                    selectable={true}
-                    onSelect={i => this.toggleMySelection(i)}
-                    selected={this.state.mySelected}
-                  />
-                </div>
-              </Col>
-              <Col md="6">
-                <InputGroup>
-                  <FormControl
-                    value={this.state.newPartnerAddress}
-                    onChange={e =>
-                      this.setState({ newPartnerAddress: e.target.value })
-                    }
-                  />
-
-                  <InputGroup.Append>
-                    <Button
-                      variant="secondary"
-                      className=""
-                      onClick={() => {
-                        history.push("/trade/" + this.state.newPartnerAddress);
-                        this.setState(
-                          {
-                            partnerAddress: this.state.newPartnerAddress
-                          },
-                          () => {
-                            this.refreshPartnerInventory();
-                          }
-                        );
-                      }}
-                    >
-                      Load
-                    </Button>
-                  </InputGroup.Append>
-                </InputGroup>
-                <div className="mt-2">
-                  <Inventory
-                    items={this.state.partnerAssets}
-                    selectable={true}
-                    onSelect={i => this.togglePartnerSelection(i)}
-                    selected={this.state.partnerSelected}
-                  />
-                </div>
-              </Col>
-            </Row>
-            <Row style={{ textAlign: "right" }}>
-              <Col>
-                <Button
-                  variant="success"
-                  className="mt-2"
-                  disabled={
-                    this.state.mySelected.length +
-                      this.state.partnerSelected.length ===
-                      0 || this.state.partnerAddress === ""
+    return (
+      <div className="NewTradeView">
+        <Container className="mt-4">
+          <Row>
+            <Col>
+              {React.createElement(
+                withRouter(({ history }) => (
+                  <Button
+                    variant="danger"
+                    className=""
+                    onClick={() => history.push("/offers")}
+                  >
+                    Back
+                  </Button>
+                ))
+              )}
+            </Col>
+          </Row>
+          <Row className="mt-2">
+            <Col md="6">
+              <InputGroup>
+                <FormControl disabled value={this.state.myAddress} readOnly />
+                <InputGroup.Append>
+                  <Button
+                    variant="secondary"
+                    onClick={() => this.refreshMyInventory()}
+                  >
+                    Refresh
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+              <div className="mt-2">
+                <Inventory
+                  items={this.state.myAssets}
+                  selectable={true}
+                  onSelect={i => this.toggleMySelection(i)}
+                  selected={this.state.mySelected}
+                />
+              </div>
+            </Col>
+            <Col md="6">
+              <InputGroup>
+                <FormControl
+                  value={this.state.newPartnerAddress}
+                  onChange={e =>
+                    this.setState({ newPartnerAddress: e.target.value })
                   }
-                  onClick={async () => {
-                    const account = (await window.web3.eth.getAccounts())[0];
-                    Swal.fire({
-                      title: "Sending trade offer...",
-                      showCancelButton: false,
-                      showConfirmButton: false,
-                      onBeforeOpen: () => {
-                        Swal.showLoading();
-                      }
-                    });
-                    GTS.methods
-                      .sendTradeOffer(
-                        this.state.partnerAddress,
-                        this.state.mySelected,
-                        this.state.partnerSelected
-                      )
-                      .send({ from: account })
-                      .on("transactionHash", () => {
-                        Swal.fire({
-                          title: "Offer sent!",
-                          text:
-                            "It may take a while for it to appear on the Offers page.",
-                          type: "success"
-                        });
-                        history.push("/offers");
-                      })
-                      .catch(err => {
-                        Swal.fire({
-                          title: "An error occurred.",
-                          text: err,
-                          type: "error"
-                        });
+                />
+
+                <InputGroup.Append>
+                  {React.createElement(
+                    withRouter(({ history }) => (
+                      <Button
+                        variant="secondary"
+                        className=""
+                        onClick={() => {
+                          history.push(
+                            "/trade/" + this.state.newPartnerAddress
+                          );
+                          this.setState(
+                            {
+                              partnerAddress: this.state.newPartnerAddress
+                            },
+                            () => {
+                              this.refreshPartnerInventory();
+                            }
+                          );
+                        }}
+                      >
+                        Load
+                      </Button>
+                    ))
+                  )}
+                </InputGroup.Append>
+              </InputGroup>
+              <div className="mt-2">
+                <Inventory
+                  items={this.state.partnerAssets}
+                  selectable={true}
+                  onSelect={i => this.togglePartnerSelection(i)}
+                  selected={this.state.partnerSelected}
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row style={{ textAlign: "right" }}>
+            <Col>
+              {React.createElement(
+                withRouter(({ history }) => (
+                  <Button
+                    variant="success"
+                    className="mt-2"
+                    disabled={
+                      this.state.mySelected.length +
+                        this.state.partnerSelected.length ===
+                        0 || this.state.partnerAddress === ""
+                    }
+                    onClick={async () => {
+                      const account = (await window.web3.eth.getAccounts())[0];
+                      Swal.fire({
+                        title: "Sending trade offer...",
+                        showCancelButton: false,
+                        showConfirmButton: false,
+                        onBeforeOpen: () => {
+                          Swal.showLoading();
+                        }
                       });
-                  }}
-                >
-                  Send
-                </Button>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      ))
+                      GTS.methods
+                        .sendTradeOffer(
+                          this.state.partnerAddress,
+                          this.state.mySelected,
+                          this.state.partnerSelected
+                        )
+                        .send({ from: account })
+                        .on("transactionHash", () => {
+                          Swal.fire({
+                            title: "Offer sent!",
+                            text:
+                              "It may take a while for it to appear on the Offers page.",
+                            type: "success"
+                          });
+                          history.push("/offers");
+                        })
+                        .catch(err => {
+                          Swal.fire({
+                            title: "An error occurred.",
+                            text: err,
+                            type: "error"
+                          });
+                        });
+                    }}
+                  >
+                    Send
+                  </Button>
+                ))
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
   toggleMySelection(i) {
